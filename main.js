72a37d5056e47b8a72ca2e6b60c202c36e4b73e2
Precidia <precidia.tom@gmail.com>
2018-04-15 23:27:02 -0700

    initalized

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 
+var app = express();
+app.set('view engine', 'hbs');
 app.get('/', (request, response) => {
 
 	response.render('main.hbs', {
 		title: 'Main'
 	});
 });
+
+function checkRecords() {
+	if (fs.existsSync('userpass.json') && fs.readFileSync('userpass.json').length !== 0) {
+    	getFile = fs.readFileSync('userpass.json');
+    	Records = JSON.parse(getFile);
+	}
+}
 
 app.listen(port, () => {
 	console.log(`Server is on port ${port}`);
