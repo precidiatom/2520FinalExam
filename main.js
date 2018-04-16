43dd6972139fefa5fa70e894638f9f31ba169384
Precidia <precidia.tom@gmail.com>
2018-04-15 23:45:10 -0700

    test feat1 page

--- main.js
+++ main.js
@@ -6,11 +6,35 @@ const port = process.env.PORT || 8080;
 
 var app = express();
 app.set('view engine', 'hbs');
-app.get('/', (request, response) => {
+app.use(express.static(__dirname + '/views'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(bodyParser.urlencoded({ 
+	extended:true
+}));
 
+var Records = {};
+
+app.get('/', (request, response) => {
 	response.render('main.hbs', {
 		title: 'Main'
 	});
+});
+
+app.get('/feature1', (request, response) => {
+	response.render('feature1.hbs', {
+		title: 'Feature 1'
+	})
+})
+
+app.post('/featureoneform', (request, response) => {
+	var record = {
+		"username": request.body.username,
+		"password": request.body.password
+	};
+	
+	Records.push(record);
+	updatedRecords = JSON.stringify(chefRecords);
+	fs.writeFileSync('userpass.json', updatedRecords); 
 });
 
 function checkRecords() {
