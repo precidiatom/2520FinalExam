8b1f3a5cb874892603c4e3b1b782c2c2926d678f
Precidia <precidia.tom@gmail.com>
2018-04-15 21:03:53 -0700

    editted main

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 8080;
 
+app.get('/', (request, response) => {
+
+	response.render('main.hbs', {
+		title: 'Main'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is on port ${port}`);
 })
